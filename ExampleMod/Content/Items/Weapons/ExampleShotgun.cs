8c26e22bdd11b7fc4ec11140f75dc22c49413c66
Stinger200916 <arseniymukhin2008@gmail.com>
2025-03-31 15:17:42 +0300

    WormsShotgun.cs

--- ExampleMod/Content/Items/Weapons/ExampleShotgun.cs
+++ ExampleMod/Content/Items/Weapons/ExampleShotgun.cs
@@ -25,7 +25,7 @@ namespace ExampleMod.Content.Items.Weapons
 
 			// Weapon Properties
 			Item.DamageType = DamageClass.Ranged; // Sets the damage type to ranged.
-			Item.damage = 10; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			Item.damage = 25; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
 			Item.knockBack = 6f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			Item.noMelee = true; // So the item's animation doesn't do damage.
 
